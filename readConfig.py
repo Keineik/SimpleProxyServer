7e110d8072ded9839af84e36143b824a664d1a6c
Văn Bá Đức Kiên <61812509+Keineik@users.noreply.github.com>
2023-08-09 22:46:58 +0700

    Delete readConfig.py

--- readConfig.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def getconfig():
-    fileConfig = open('fileconfig.json')
-    configs = json.load(fileConfig) 
-    return configs['cache_time'], configs['whitelist'], configs['time']
